7ee34441c879371de9261372aedac64828038f2e
anjuncc <anjuncc@163.com>
2016-03-23 18:19:15 +0800

    a

---
+++ avplayer/avplayer-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  avplayer-Bridging-Header.h
+//  avplayer
+//
+//  Created by anjun on 16/3/23.
+//  Copyright © 2016年 cptrue. All rights reserved.
+//
+
+#ifndef avplayer_Bridging_Header_h
+#define avplayer_Bridging_Header_h
+#import "MBProgressHUD.h"
+#import "SVProgressHUD.h"
+#endif /* avplayer_Bridging_Header_h */
